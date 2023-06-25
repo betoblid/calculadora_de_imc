4e48ad26537bb1f5b17186c766822b4b33c37ec5
dev-herbert <soyzabeto@gmail.com>
2023-06-25 00:47:39 -0300

    comentando no script

--- calcular.js
+++ calcular.js
@@ -1,22 +1,32 @@
+
+//selecionando elementos no Dom
 const peso = document.getElementById("cxpeso")
 const altura = document.getElementById("cxaltura")
 let cxtext = document.getElementById("valor_imc")
 let cxalert = document.getElementById("aviso")
 const box_resultado = document.getElementById("box-resu")
 const fecha = document.getElementById("button_fecha")
-// apenas para nao enviar o formulario
-//tive que por aqui senão daria conflito
 const enviar = document.getElementById("envia")
+
+//function que tera uma variavel de controle que decidirar se irar procegui
+let continuar = false;
+
+//essa function será acionada quando o evento click ser acionando
+//ela será responsavel por calcular a media do aluno
 enviar.addEventListener("click", function (event) {
+    //esse method proibi o carregamento da pagina quando se envia um formulario
     event.preventDefault()
 
-    validar_cx()
+    //function que será chamada para falidar os resultados do formulario
+    validar_form()
+
     //chamar o card resultado
-    if(continuar == true){
+    if(continuar){
         chamaresult()
     }
 })
 //function para feixar a mensagem com os resultados
+//butão fechar
 fecha.addEventListener("click", function () {
     if(box_resultado.classList.contains("on")){
         box_resultado.classList.remove("on")
@@ -28,21 +38,23 @@ fecha.addEventListener("click", function () {
 
     }
 })
-
-function chamaresult (){
+//function que chamará o resultado esse resultado é passado numa box e aparece sobre o form
+function chamaresult(){
+    //condiçao caso tiver o hidden será tirado e se não tiver colocara
     if(box_resultado.classList.contains("hidden")){
         box_resultado.classList.remove("hidden")
         box_resultado.classList.add("on")
     }else{
         box_resultado.classList.add("hidden")
     }
+    //function que dará os valores pra box
     valor()
 }
-//function que tera uma variavel de controle que decidirar se irar procegui
-let continuar = false;
 
-function validar_cx(){
+//funcyion que valida formulario caso o usuario não tiver digitado valores
+function validar_form(){
     
+    //condição que verificará se digitarão algo
     if(String(peso.value) == ""){
         alert("digite seu peso")
     } if((String(altura.value) == "")){
@@ -52,18 +64,25 @@ function validar_cx(){
     }
 }
 
+//fuction que passará todos os valores no Dom
 function valor () {
+    //faz a conta do IMC e armazena na variavel
     let imc = peso.value / (altura.value * altura.value)
+    //passa os resultados no DOm
     document.querySelector("#result_text").textContent = verific_imc(imc)
+    
     document.querySelector("#result_imc").textContent = imc.toFixed(1)
     cxalert.textContent = verific_imc(imc)
     cxtext.textContent = imc.toFixed(1)
 }
 
-
+//function responsavel por verificar se o IMC está bom para o peso da pessoa
+//ou está passando do limite
 function verific_imc(imc){
+    //variavel para armazenar valor passado pela condição
     let status = ""
 
+    //condição da verificação
     if(imc < 17){
         status = "Muito abaixo do peso"
     } else if(imc == 17 || imc <= 18.49){
@@ -81,6 +100,7 @@ function verific_imc(imc){
     } else{
         status = "tente de novo!"
     }
+    //retorno que essa function passará
     return status;
 }
 	
